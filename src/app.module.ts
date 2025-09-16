bd3bf478f7e4127893a2ea859d4182ecaa2ba56d
tokenlab <igorsilva@tokenlab.com.br>
2025-09-16 11:02:11 -0300

    feat: Criação e configuração do projeto backend

---
+++ src/app.module.ts
@@ -0,0 +1,26 @@
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { ClassSerializerInterceptor, Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { DbConfigService } from './config/db.config.service';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: DbConfigService,
+      inject: [DbConfigService],
+    }),
+  ],
+  controllers: [],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ClassSerializerInterceptor,
+    },
+  ],
+})
+export class AppModule {}
