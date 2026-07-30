9390c0ae7f776e73e24b717ccdce6e46b6da7384
igorjung <igorc.jung@gmail.com>
2026-07-30 15:46:30 -0300

    refactor(config): Refatorar seeds e url do arquivo main

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://tcc-frontend-livid.vercel.app'],
+    origin: [process.env.APP_URL],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
